5f44f1ba4bd1af8ce4d1e914be41a2c543a38362
Ivo Raisr <ivosh@ivosh.net>
2017-10-26 04:35:02 +0200

    Proper sizing of StartCisloBox component.

--- src/StartCislo/StartCisloBox.js
+++ src/StartCislo/StartCisloBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './StartCisloBox.css';
 
-const StartCisloBox = ({ cislo }) => <span className="StartCisloBox">{cislo}</span>;
+const StartCisloBox = ({ cislo }) => <div className="StartCisloBox">{cislo}</div>;
 
 StartCisloBox.propTypes = {
   cislo: PropTypes.number.isRequired
