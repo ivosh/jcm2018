30cd72e760f8170c8f668d91817ba1b053d76622
Ivo Raisr <ivosh@ivosh.net>
2017-11-09 17:30:07 +0100

    New test case for creating dvě účasti.

--- common/index.js
+++ common/index.js
@@ -7,7 +7,8 @@ const FIND_UCAST_BY_UCASTNIK = 'find_ucast_by_ucastnik';
 const CODE_OK = 'ok';
 const CODE_ALREADY_EXISTING = 'již existuje';
 const CODE_NONEXISTING = 'neexistuje';
-const CODE_UNPARSABLE_MESSAGE = 'unparsable message';
+const CODE_UNFULFILLED_REQUEST = 'unfulfilled request';
+const CODE_UNPARSEABLE_MESSAGE = 'unparseable message';
 const CODE_UNRECOGNIZED_ACTION = 'unrecognized action';
 
 const createUcast = ({ id, rok, udaje, prihlaska }) => ({
@@ -27,7 +28,8 @@ module.exports = {
   CODE_OK,
   CODE_ALREADY_EXISTING,
   CODE_NONEXISTING,
-  CODE_UNPARSABLE_MESSAGE,
+  CODE_UNFULFILLED_REQUEST,
+  CODE_UNPARSEABLE_MESSAGE,
   CODE_UNRECOGNIZED_ACTION,
   createUcast,
   findAllUcastnici
--- server/api/Ucastnik/createUcast.test.js
+++ server/api/Ucastnik/createUcast.test.js
@@ -47,4 +47,30 @@ it('vytvoř minimálního účastníka', async () => {
 
   const ucastnici = await Ucastnik.find({}, { _id: 0 });
   expect(ucastnici).toMatchSnapshot();
+
+  await Ucastnik.remove({});
+});
+
+it('vytvoř dvě účasti', async () => {
+  const udaje1 = {
+    prijmeni: 'Balabák',
+    jmeno: 'František',
+    narozeni: { rok: 1953 },
+    pohlavi: 'muz',
+    obec: 'Ostrava 1'
+  };
+  const udaje2 = { ...udaje1, obec: 'Ostrava 2' };
+
+  const response1 = await wsClient.sendRequest(Actions.createUcast({ rok: 2017, udaje: udaje1 }));
+
+  const ucastnikId = response1.response.id;
+  const { requestId, ...response2 } = await wsClient.sendRequest(
+    Actions.createUcast({ id: ucastnikId, rok: 2018, udaje: udaje2 })
+  );
+  expect(response2).toMatchSnapshot();
+
+  const ucastnici = await Ucastnik.find({}, { _id: 0 });
+  expect(ucastnici).toMatchSnapshot();
+
+  await Ucastnik.remove({});
 });
