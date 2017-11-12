c67fa94b93f4ab401cbb084b2367de5fe75f3bbf
Ivo Raisr <ivosh@ivosh.net>
2017-11-12 18:34:25 +0100

    Flesh out test cases for findAllUcastnici.

--- server/api/Ucastnik/findAllUcastnici.test.js
+++ server/api/Ucastnik/findAllUcastnici.test.js
@@ -4,6 +4,7 @@ const db = require('../../db');
 const Actions = require('../../../common');
 const createWsServer = require('../../createWsServer');
 const createWsClient = require('../createWsClient');
+const Ucastnik = require('../../model/Ucastnik');
 
 const port = 5602;
 const wsServer = createWsServer({});
@@ -24,6 +25,62 @@ afterAll(async () => {
 });
 
 it('findAllUcastnici', async () => {
-  // :TODO: create some first
-  await wsClient.sendRequest(Actions.findAllUcastnici());
+  const ucasti1 = [
+    {
+      rok: 2017,
+      udaje: {
+        prijmeni: 'Balabák',
+        jmeno: 'Roman',
+        narozeni: { rok: 1956 },
+        pohlavi: 'muz',
+        obec: 'Ostrava 1'
+      }
+    },
+    {
+      rok: 2018,
+      udaje: {
+        prijmeni: 'Balabák',
+        jmeno: 'Roman',
+        narozeni: { rok: 1956 },
+        pohlavi: 'muz',
+        obec: 'Ostrava 2'
+      }
+    }
+  ];
+
+  const ucastnik1 = new Ucastnik({ ucasti: ucasti1 });
+  await ucastnik1.save();
+
+  const ucasti2 = [
+    {
+      rok: 2016,
+      udaje: {
+        prijmeni: 'Sukdoláková',
+        jmeno: 'Zdeňka',
+        narozeni: { den: 7, mesic: 12, rok: 1966 },
+        pohlavi: 'zena',
+        obec: 'Kladno'
+      }
+    },
+    {
+      rok: 2017,
+      udaje: {
+        prijmeni: 'Primasová',
+        jmeno: 'Zdeňka',
+        narozeni: { den: 7, mesic: 12, rok: 1966 },
+        pohlavi: 'zena',
+        obec: 'Kladno'
+      }
+    }
+  ];
+
+  const ucastnik2 = new Ucastnik({ ucasti: ucasti2 });
+  await ucastnik2.save();
+
+  const { requestId, ...response } = await wsClient.sendRequest(Actions.findAllUcastnici());
+  expect(response.response[0]._id).not.toBeNull();
+  expect(response.response[1]._id).not.toBeNull();
+  response.response[0]._id = '---';
+  response.response[1]._id = '---';
+  expect(response).toMatchSnapshot();
 });
