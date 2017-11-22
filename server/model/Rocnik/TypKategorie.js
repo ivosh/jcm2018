c8274e5008e9ab31091d9a508586f679a7fc2428
Ivo Raisr <ivosh@ivosh.net>
2017-11-22 23:47:42 +0100

    Prevent duplicates in kategorie array; allow duplicates in different TypKategorie, though.

--- server/model/Rocnik/TypKategorie.js
+++ server/model/Rocnik/TypKategorie.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const uniqueArrayPlugin = require('mongoose-unique-array');
 const Startovne = require('./Startovne');
 
 const TypKategorieSchema = new mongoose.Schema(
@@ -10,11 +11,14 @@ const TypKategorieSchema = new mongoose.Schema(
       enum: ['maraton', 'půlmaraton', 'cyklo', 'koloběžka', 'pěší'],
       required: true
     },
-    kategorie: [{ type: mongoose.Schema.ObjectId, ref: 'Kategorie', unique: true }],
+    kategorie: [{ type: mongoose.Schema.ObjectId, ref: 'Kategorie' }],
     startCisla: { type: String },
     startovne: { type: Startovne, required: true }
   },
   { _id: false, bufferCommands: false }
 );
+
+// Prevent duplicates in kategorie array.
+TypKategorieSchema.plugin(uniqueArrayPlugin);
 
 module.exports = TypKategorieSchema;
