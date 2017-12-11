354c0cbe7e1f6efb2cfd626ac51648124cd698a2
Ivo Raisr <ivosh@ivosh.net>
2017-12-11 23:38:01 +0100

    Simulate button actions for Stopky.

--- ui/src/casomeric/Stopky/Stopky.test.js
+++ ui/src/casomeric/Stopky/Stopky.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import Stopky from './Stopky';
 
-const fakeOnStart = () => ({});
-const fakeOnAddMezicas = () => ({});
-const fakeOnStop = () => ({});
-
-it('před startem', () => {
+it('snapshot před startem', () => {
   const component = renderer.create(
     <Stopky
       running={false}
@@ -14,15 +11,15 @@ it('před startem', () => {
       startEnabled={true}
       mezicasEnabled={false}
       stopEnabled={false}
-      onStart={fakeOnStart}
-      onAddMezicas={fakeOnAddMezicas}
-      onStop={fakeOnStop}
+      onStart={jest.fn()}
+      onAddMezicas={jest.fn()}
+      onStop={jest.fn()}
     />
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
-it('po startu', () => {
+it('snapshort po startu', () => {
   const component = renderer.create(
     <Stopky
       running={true}
@@ -30,26 +27,70 @@ it('po startu', () => {
       startEnabled={false}
       mezicasEnabled={true}
       stopEnabled={true}
-      onStart={fakeOnStart}
-      onAddMezicas={fakeOnAddMezicas}
-      onStop={fakeOnStop}
+      onStart={jest.fn()}
+      onAddMezicas={jest.fn()}
+      onStop={jest.fn()}
     />
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
-it('po stopce', () => {
-  const component = renderer.create(
+it('maps button Start to dispatch onStart action', () => {
+  const onStart = jest.fn();
+  const wrapper = mount(
     <Stopky
       running={false}
-      base={new Date()}
+      base={null}
       startEnabled={true}
       mezicasEnabled={false}
       stopEnabled={false}
-      onStart={fakeOnStart}
-      onAddMezicas={fakeOnAddMezicas}
-      onStop={fakeOnStop}
+      onStart={onStart}
+      onAddMezicas={jest.fn()}
+      onStop={jest.fn()}
     />
   );
-  expect(component.toJSON()).toMatchSnapshot();
+  expect(wrapper.find('button.btn-success')).toHaveLength(1);
+  wrapper.find('button.btn-success').simulate('click');
+
+  expect(onStart).toHaveBeenCalledWith(expect.any(Date));
+});
+
+it('maps button Mezičas to dispatch onAddMezicas action', () => {
+  const onAddMezicas = jest.fn();
+  const wrapper = mount(
+    <Stopky
+      running={true}
+      base={new Date()}
+      startEnabled={false}
+      mezicasEnabled={true}
+      stopEnabled={true}
+      onStart={jest.fn()}
+      onAddMezicas={onAddMezicas}
+      onStop={jest.fn()}
+    />
+  );
+  expect(wrapper.find('button.btn-info')).toHaveLength(1);
+  wrapper.find('button.btn-info').simulate('click');
+
+  expect(onAddMezicas).toHaveBeenCalled();
+});
+
+it('maps button Stop to dispatch onStop action', () => {
+  const onStop = jest.fn();
+  const wrapper = mount(
+    <Stopky
+      running={true}
+      base={new Date()}
+      startEnabled={false}
+      mezicasEnabled={true}
+      stopEnabled={true}
+      onStart={jest.fn()}
+      onAddMezicas={jest.fn()}
+      onStop={onStop}
+    />
+  );
+  expect(wrapper.find('button.btn-danger')).toHaveLength(1);
+  wrapper.find('button.btn-danger').simulate('click');
+
+  expect(onStop).toHaveBeenCalled();
 });
