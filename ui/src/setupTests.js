c28de6efbcf6ad2866f9c79403acdad46819a988
Ivo Raisr <ivosh@ivosh.net>
2017-12-13 22:28:55 +0100

    Disable some eslint warnings.

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* This file is used only by jest test framework. */
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-localstorage-mock';
+/* eslint-enable import/no-extraneous-dependencies */
 
 configure({ adapter: new Adapter() });
