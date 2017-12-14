5e2f52bc5aba945bc020f782aada2bde6e715198
Ivo Raisr <ivosh@ivosh.net>
2017-12-14 17:10:24 +0100

    Add tests for UcastniciDigestContainer.

---
+++ ui/src/registrator/UcastniciDigest/UcastniciDigestContainer.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { SortDirTypes } from './ucastniciDigestReducer';
+import UcastniciDigestContainer from './UcastniciDigestContainer';
+
+const mockStore = configureStore();
+
+let store;
+let wrapper;
+beforeEach(() => {
+  const state = {
+    ucastnici: {
+      allIds: ['6f09b1fd371dec1e99b7e1c9', '5a09b1fd371dec1e99b7e1c9'],
+      byIds: {
+        '6f09b1fd371dec1e99b7e1c9': {
+          roky: [2016],
+          2016: {
+            udaje: {
+              prijmeni: 'Sukdoláková',
+              jmeno: 'Martina',
+              narozeni: { rok: 1963 },
+              pohlavi: 'zena',
+              obec: 'Zlín',
+              stat: 'Česká republika'
+            }
+          }
+        },
+        '5a09b1fd371dec1e99b7e1c9': {
+          roky: [2018, 2017],
+          2018: {
+            udaje: {
+              prijmeni: 'Balabák',
+              jmeno: 'Roman',
+              narozeni: { rok: 1956, mesic: 5, den: 15 },
+              pohlavi: 'muz',
+              obec: 'Ostrava 2',
+              stat: 'Česká republika'
+            }
+          },
+          2017: {
+            udaje: {
+              prijmeni: 'Balabák',
+              jmeno: 'Roman',
+              narozeni: { rok: 1956 },
+              pohlavi: 'muz',
+              obec: 'Ostrava 1',
+              stat: 'Česká republika'
+            }
+          }
+        }
+      }
+    },
+    registrator: { ucastniciDigest: { sortColumn: 'prijmeni', sortDir: SortDirTypes.DESC } }
+  };
+  store = mockStore(state);
+  store.dispatch = jest.fn();
+  wrapper = shallow(
+    <UcastniciDigestContainer store={store} containerWidth={500} containerHeight={500} />
+  );
+});
+
+it('maps state and dispatch to props', () => {
+  expect(wrapper.props().ucastniciDigest).toBeTruthy();
+  expect(wrapper.props().ucastniciDigest).toMatchSnapshot();
+  expect(wrapper.props().sortColumn).toEqual('prijmeni');
+  expect(wrapper.props().sortDir).toEqual('desc');
+});
+
+it('maps onSortDirChange to dispatch sortDirChange action', () => {
+  wrapper.props().onSortDirChange('jmeno');
+
+  expect(store.dispatch).toHaveBeenCalledWith({
+    type: 'UCASTNICI_DIGEST_SORT_DIR_CHANGE',
+    sortColumn: 'jmeno'
+  });
+});
