ba95ed6852752a444fdc7648ff2057a5dddb3e5b
Ivo Raisr <ivosh@ivosh.net>
2017-12-24 22:08:19 +0100

    Some tests for correct routing setup within index.js and App component.

--- ui/src/index.js
+++ ui/src/index.js
@@ -10,35 +10,44 @@ import AppContainer from './App/AppContainer';
 import { websocketConnected, websocketDisconnected } from './App/AppActions';
 import './index.css';
 
-const wsClient = new WsClient();
-const store = configureStore(wsClient);
+const setupStoreAndWsClient = () => {
+  const wsClient = new WsClient();
+  const store = configureStore(wsClient);
 
-wsClient.setCallbacks({
-  onConnect: () => {
-    store.dispatch(websocketConnected());
-    // store.dispatch(fetchRocniky());
-  },
-  onClose: () => store.dispatch(websocketDisconnected())
-});
+  wsClient.setCallbacks({
+    onConnect: () => {
+      store.dispatch(websocketConnected());
+      // store.dispatch(fetchRocniky());
+    },
+    onClose: () => store.dispatch(websocketDisconnected())
+  });
 
-try {
-  /* Start asynchronous connect to the websocket server.
+  try {
+    /* Start asynchronous connect to the websocket server.
      WsClient will retry indefinitely if the connection cannot be established. */
-  wsClient.connect();
-} catch (err) {
-  // Silently ignore any errors. They should have been dispatched from WsClient anyway.
-}
+    wsClient.connect();
+  } catch (err) {
+    // Silently ignore any errors. They should have been dispatched from WsClient anyway.
+  }
 
-/* Render a pathless <Route> so that 'location' is automatically injected as a 'prop'
+  return store;
+};
+
+export const renderApp = store => {
+  /* Render a pathless <Route> so that 'location' is automatically injected as a 'prop'
    into AppContainer and causes re-render on location change. See:
    https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/guides/blocked-updates.md
  */
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Route component={AppContainer} />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route component={AppContainer} />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+const store = setupStoreAndWsClient();
+renderApp(store);
 registerServiceWorker();
---
+++ ui/src/index.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import AppContainer from './App/AppContainer';
+
+const mockStore = configureStore();
+const store = mockStore({
+  casomeric: { mezicasy: [], stopky: { base: null, running: false } },
+  registrator: { ucastniciDigest: { filter: '', sortColumn: undefined, sortDir: 'none' } },
+  startujici: [],
+  ucastnici: { allIds: [], byIds: {} },
+  connected: true
+});
+
+test('navigates to /casomeric by default', () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Route component={AppContainer} />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  expect(wrapper.find('Stopky')).toHaveLength(1);
+});
+
+test('navigates correctly to /registrace', () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[ '/registrace' ]} >
+        <Route component={AppContainer} />
+      </MemoryRouter>
+    </Provider>
+  );
+
+  expect(wrapper.find('Registrace')).toHaveLength(1);
+});
