5a643bca36acc5dad582a10c319f2189f166bbcf
Ivo Raisr <ivosh@ivosh.net>
2017-12-27 12:56:43 +0100

    Close the database connection even on XML file import error.

--- server/xmlImportFile.js
+++ server/xmlImportFile.js
@@ -4,16 +4,21 @@ const logger = require('heroku-logger');
 const db = require('./db');
 const importFromXML = require('./model/importFromXML');
 
-const file = process.argv[2];
+const file = process.argv[2] || '<no file specified>';
 process.title = 'xml-import';
 
 const main = async () => {
   await db.connect();
   await db.dropDatabase();
   logger.info(`Importing ${file}.`);
-  await importFromXML({ file });
-  logger.info('Import completed.');
-  await db.disconnect();
+  try {
+    await importFromXML({ file });
+    logger.info('Import completed.');
+  } catch (err) {
+    throw err;
+  } finally {
+    await db.disconnect();
+  }
 };
 
 main()
