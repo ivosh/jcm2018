8ff2f920dc31d925b761fb43f63c62f342e8da2d
Ivo Raisr <ivosh@ivosh.net>
2018-01-15 17:38:29 +0100

    Add tests for prihlaseniReducer.

---
+++ ui/src/registrator/Prihlaseni/prihlaseniReducer.test.js
@@ -0,0 +1,110 @@
+import deepFreeze from 'deep-freeze';
+import { hideError, reset } from './PrihlaseniActions';
+import prihlaseniReducer from './prihlaseniReducer';
+
+it('na začátku', () => {
+  const stateBefore = undefined;
+
+  const stateAfter = prihlaseniReducer(stateBefore, {});
+  expect(stateAfter.errorCode).toEqual('');
+  expect(stateAfter.errorMessage).toEqual('');
+  expect(stateAfter.showError).toBe(false);
+  expect(stateAfter.fetching).toBe(false);
+  expect(stateAfter.saving).toBe(false);
+  expect(stateAfter.ucastnikId).toBe(undefined);
+  expect(stateAfter.validateEmpty).toBe(false);
+  expect(stateAfter.udaje).toEqual({
+    prijmeni: undefined,
+    jmeno: undefined,
+    narozeni: { den: undefined, mesic: undefined, rok: undefined },
+    pohlavi: undefined,
+    adresa: undefined,
+    obec: undefined,
+    psc: undefined,
+    stat: 'Česká republika',
+    klub: undefined,
+    email: undefined,
+    telefon: undefined
+  });
+  expect(stateAfter.prihlaska).toEqual({
+    datum: undefined,
+    kategorie: undefined,
+    startCislo: undefined,
+    kod: undefined
+  });
+});
+
+it('hideError()', () => {
+  const stateBefore = {
+    saving: false,
+    errorCode: 'database problem',
+    errorMessage: 'Problém při komunikaci s databází.',
+    showError: true
+  };
+  const stateAfter = { ...stateBefore, showError: false };
+  deepFreeze(stateBefore);
+
+  expect(prihlaseniReducer(stateBefore, hideError())).toEqual(stateAfter);
+});
+
+it('reset()', () => {
+  const stateBefore = {
+    errorCode: 'chybový kód',
+    errorMessage: 'Dlouhá chybová hláška.',
+    showError: true,
+    fetching: false,
+    saving: true,
+    ucastnikId: '===id===',
+    validateEmpty: true,
+    udaje: {
+      prijmeni: 'Hudák',
+      jmeno: 'Jan',
+      narozeni: { den: 7, mesic: 7, rok: 1954 },
+      pohlavi: 'muž',
+      adresa: '',
+      obec: 'Samorost',
+      psc: '34567',
+      stat: 'Česká republika',
+      klub: '',
+      email: 'em@ai.l',
+      telefon: '123 456 789'
+    },
+    prihlaska: {
+      datum: '1. 10. 2019',
+      kategorie: 'maraton',
+      startCislo: 23,
+      kod: '===kod=='
+    }
+  };
+  const stateAfter = {
+    errorCode: '',
+    errorMessage: '',
+    showError: false,
+    fetching: false,
+    saving: false,
+    ucastnikId: undefined,
+    validateEmpty: false,
+    udaje: {
+      prijmeni: undefined,
+      jmeno: undefined,
+      narozeni: { den: undefined, mesic: undefined, rok: undefined },
+      pohlavi: undefined,
+      adresa: undefined,
+      obec: undefined,
+      psc: undefined,
+      stat: 'Česká republika',
+      klub: undefined,
+      email: undefined,
+      telefon: undefined
+    },
+    prihlaska: {
+      datum: undefined,
+      kategorie: undefined,
+      startCislo: undefined,
+      kod: undefined
+    }
+  };
+  deepFreeze(stateBefore);
+
+  expect(prihlaseniReducer(stateBefore, reset())).toEqual(stateAfter);
+});
