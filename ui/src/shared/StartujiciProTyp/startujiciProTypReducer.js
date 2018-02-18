48f9007f53ed0c3ebb50dd23fd0dd9f478356308
Ivo Raisr <ivosh@ivosh.net>
2018-02-18 06:14:01 +0100

    Functions getStartujiciProTyp and getStartCislaProTyp.

---
+++ ui/src/shared/StartujiciProTyp/startujiciProTypReducer.js
@@ -0,0 +1,92 @@
+import moment from 'moment';
+import { AKTUALNI_ROK } from '../../constants';
+import { getTypKategorie } from '../../entities/rocniky/rocnikyReducer';
+
+const findStartCislo = (startujici, startCislo) =>
+  startujici.find(element => element.startCislo === startCislo);
+
+export const getStartujiciProTyp = ({
+  prihlaseni,
+  rok = AKTUALNI_ROK,
+  typ,
+  kategorie,
+  ucastnici
+}) => {
+  const results = [];
+
+  ucastnici.allIds.forEach(id => {
+    const ucastnik = ucastnici.byIds[id];
+    if (ucastnik[rok]) {
+      const { prihlaska, vykon } = ucastnik[rok];
+
+      if (prihlaseni && prihlaska && prihlaska.startCislo) {
+        const { kategorie: kategorieId, startCislo } = prihlaska;
+        if (typ === kategorie[kategorieId].typ) {
+          results.push({ id, startCislo });
+        }
+      } else if (vykon && vykon.startCislo) {
+        const { cas, dokonceno, kategorie: kategorieId, startCislo } = vykon;
+        if (typ === kategorie[kategorieId].typ) {
+          const result = { id, dokonceno, startCislo };
+          if (cas) {
+            result.cas = moment.duration(cas);
+          }
+          results.push(result);
+        }
+      }
+    }
+  });
+
+  return results.sort((a, b) => a.startCislo - b.startCislo);
+};
+
+const populateRange = (start, end) => {
+  if (end >= start) {
+    return Array(end - start + 1)
+      .fill(1)
+      .map((val, index) => start + index);
+  }
+  return Array(start - end + 1)
+    .fill(1)
+    .map((val, index) => start - index);
+};
+
+export const getStartCislaProTyp = ({
+  prihlaseni,
+  rok = AKTUALNI_ROK,
+  typ,
+  kategorie,
+  rocniky,
+  ucastnici
+}) => {
+  const typKategorie = getTypKategorie({ rok, typ, rocniky });
+  const { startCisla } = typKategorie;
+  if (!startCisla) {
+    return [];
+  }
+
+  const startujici = getStartujiciProTyp({ prihlaseni, rok, typ, kategorie, ucastnici });
+  const results = [];
+  startCisla.rozsahy.forEach(rozsah => {
+    let range = [];
+
+    const parsed = rozsah.match(/(\d+)-(\d+)/);
+    if (parsed) {
+      range = populateRange(parseInt(parsed[1], 10), parseInt(parsed[2], 10));
+    } else {
+      range = [parseInt(rozsah, 10)];
+    }
+    const result = range.map(cislo => findStartCislo(startujici, cislo) || { startCislo: cislo });
+    results.push(...result);
+  });
+
+  return results;
+};
+
+export const decorareStartujici = (startujici, filter, onClick) =>
+  startujici.map(jeden => {
+    if (filter(jeden)) {
+      return { ...jeden, onClick: () => onClick(jeden) };
+    }
+    return jeden;
+  });
---
+++ ui/src/shared/StartujiciProTyp/startujiciProTypReducer.test.js
@@ -0,0 +1,188 @@
+import deepFreeze from 'deep-freeze';
+import moment from 'moment';
+import ucastniciTestData from '../../entities/ucastnici/ucastniciTestData';
+import { getStartCislaProTyp, getStartujiciProTyp } from './startujiciProTypReducer';
+
+const state = JSON.parse(JSON.stringify(ucastniciTestData)); // deep copy
+state.entities.rocniky.byRoky[2013].kategorie.maraton.startCisla.rozsahy = ['10-15', '23', '35-36'];
+state.entities.rocniky.byRoky[2018].kategorie['koloběžka'].startCisla.rozsahy = ['15-10', '16'];
+
+state.entities.ucastnici = {
+  allIds: ['id1', 'id2', 'id3'],
+  byIds: {
+    id1: {
+      roky: [2018, 2014, 2013],
+      2013: {
+        prihlaska: {
+          kategorie: '5a587e1a051c181132cf83b8', // maraton
+          startCislo: 13
+        }
+      },
+      2014: {
+        prihlaska: {
+          kategorie: '5a587e1a051c181132cf83b8', // maraton
+          startCislo: 9
+        }
+      },
+      2018: {
+        prihlaska: {
+          kategorie: '5a587e1b051c181132cf83d0', // koloběžka
+          startCislo: 15
+        },
+        vykon: {
+          kategorie: '5a587e1b051c181132cf83d0', // koloběžka
+          startCislo: 14,
+          dokonceno: false
+        }
+      }
+    },
+    id2: {
+      roky: [2018, 2013],
+      2013: {
+        prihlaska: {
+          kategorie: '5a587e1a051c181132cf83b8', // maraton
+          startCislo: 10
+        },
+        vykon: {
+          kategorie: '5a587e1a051c181132cf83b8', // maraton
+          startCislo: 12,
+          dokonceno: true,
+          cas: 'PT2H17M23.4S'
+        }
+      },
+      2018: {
+        prihlaska: {
+          kategorie: '5a587e1a051c181132cf83b1' // pěší
+        },
+        vykon: {
+          kategorie: '5a587e1a051c181132cf83b1', // pěší
+          dokonceno: false
+        }
+      }
+    },
+    id3: {
+      roky: [2018],
+      2018: {
+        prihlaska: {
+          kategorie: '5a587e1b051c181132cf83d0', // koloběžka
+          startCislo: 10
+        },
+        vykon: {
+          kategorie: '5a587e1b051c181132cf83d0', // koloběžka
+          startCislo: 10
+        }
+      }
+    }
+  }
+};
+
+it('getStartujiciProTyp - přihlášeni 2013', () => {
+  deepFreeze(state);
+  const selected = [{ id: 'id2', startCislo: 10 }, { id: 'id1', startCislo: 13 }];
+
+  expect(
+    getStartujiciProTyp({ rok: 2013, typ: 'maraton', prihlaseni: true, ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartujiciProTyp - výkony 2013', () => {
+  deepFreeze(state);
+  const selected = [
+    { id: 'id2', startCislo: 12, dokonceno: true, cas: moment.duration('PT2H17M23.4S') }
+  ];
+
+  expect(getStartujiciProTyp({ rok: 2013, typ: 'maraton', ...state.entities })).toEqual(selected);
+});
+
+it('getStartujiciProTyp - přihlášeni 2018', () => {
+  deepFreeze(state);
+  const selected = [{ id: 'id3', startCislo: 10 }, { id: 'id1', startCislo: 15 }];
+
+  expect(
+    getStartujiciProTyp({ rok: 2018, typ: 'koloběžka', prihlaseni: true, ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartujiciProTyp - výkony 2018', () => {
+  deepFreeze(state);
+  const selected = [
+    { id: 'id3', startCislo: 10, dokonceno: undefined },
+    { id: 'id1', startCislo: 14, dokonceno: false }
+  ];
+
+  expect(
+    getStartujiciProTyp({ rok: 2018, typ: 'koloběžka', prihlaseni: false, ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartCislaProTyp - přihlášeni 2013', () => {
+  deepFreeze(state);
+  const selected = [
+    { id: 'id2', startCislo: 10 },
+    { startCislo: 11 },
+    { startCislo: 12 },
+    { id: 'id1', startCislo: 13 },
+    { startCislo: 14 },
+    { startCislo: 15 },
+    { startCislo: 23 },
+    { startCislo: 35 },
+    { startCislo: 36 }
+  ];
+
+  expect(
+    getStartCislaProTyp({ rok: 2013, typ: 'maraton', prihlaseni: true, ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartCislaProTyp - výkony 2013', () => {
+  deepFreeze(state);
+  const selected = [
+    { startCislo: 10 },
+    { startCislo: 11 },
+    { id: 'id2', startCislo: 12, dokonceno: true, cas: moment.duration('PT2H17M23.4S') },
+    { startCislo: 13 },
+    { startCislo: 14 },
+    { startCislo: 15 },
+    { startCislo: 23 },
+    { startCislo: 35 },
+    { startCislo: 36 }
+  ];
+
+  expect(
+    getStartCislaProTyp({ rok: 2013, typ: 'maraton', ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartCislaProTyp - přihlášeni 2018', () => {
+  deepFreeze(state);
+  const selected = [
+    { id: 'id1', startCislo: 15 },
+    { startCislo: 14 },
+    { startCislo: 13 },
+    { startCislo: 12 },
+    { startCislo: 11 },
+    { id: 'id3', startCislo: 10 },
+    { startCislo: 16 },
+  ];
+
+  expect(
+    getStartCislaProTyp({ rok: 2018, typ: 'koloběžka', prihlaseni: true, ...state.entities })
+  ).toEqual(selected);
+});
+
+it('getStartCislaProTyp - výkony 2018', () => {
+  deepFreeze(state);
+  const selected = [
+    { startCislo: 15 },
+    { id: 'id1', startCislo: 14, dokonceno: false },
+    { startCislo: 13 },
+    { startCislo: 12 },
+    { startCislo: 11 },
+    { id: 'id3', startCislo: 10 },
+    { startCislo: 16 },
+  ];
+
+  expect(
+    getStartCislaProTyp({ rok: 2018, typ: 'koloběžka', prihlaseni: false, ...state.entities })
+  ).toEqual(selected);
+});
