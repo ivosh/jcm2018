5c827a9719a29a4709bbd5236a1030ea29e0cb66
Ivo Raisr <ivosh@ivosh.net>
2018-03-04 04:00:30 +0100

    Do not log timestamp in production - heroku does that already.

--- server/logger.js
+++ server/logger.js
@@ -1,12 +1,6 @@
 const winston = require('winston');
 
 const formatDevelopmentParams = info => {
-  const { level, message, ...args } = info;
-
-  return `[${level}] ${message} ${Object.keys(args).length ? JSON.stringify(args, '', '') : ''}`;
-};
-
-const formatProductionParams = info => {
   const { timestamp, level, message, ...args } = info;
   const ts = timestamp.slice(0, 19).replace('T', ' ');
 
@@ -15,16 +9,20 @@ const formatProductionParams = info => {
   }`;
 };
 
+const formatProductionParams = info => {
+  const { level, message, ...args } = info;
+
+  return `[${level}] ${message} ${Object.keys(args).length ? JSON.stringify(args, '', '') : ''}`;
+};
+
 const formats = {
   development: winston.format.combine(
     winston.format.colorize(),
+    winston.format.timestamp(),
     winston.format.printf(formatDevelopmentParams)
   ),
-  production: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(formatProductionParams)
-  ),
-  test: winston.format.combine(winston.format.printf(formatDevelopmentParams))
+  production: winston.format.combine(winston.format.printf(formatProductionParams)),
+  test: winston.format.combine(winston.format.printf(formatProductionParams))
 };
 
 const env = process.env.NODE_ENV || 'development';
