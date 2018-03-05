6db097373285b04d3d7dcd7676c269ee0af6f199
Ivo Raisr <ivosh@ivosh.net>
2018-03-05 12:30:22 +0100

    Fetch ucastnici only if connected. Use the cached value otherwise.

--- ui/src/entities/ucastnici/ucastniciActions.js
+++ ui/src/entities/ucastnici/ucastniciActions.js
@@ -29,10 +29,10 @@ export const fetchUcastniciError = ({ code, status, err }) => ({
 });
 
 export const fetchUcastnici = () => async (dispatch, getState, wsClient) => {
-  const { auth, entities: { ucastnici } } = getState();
+  const { auth, connected, entities: { ucastnici } } = getState();
   if (ucastnici && ucastnici.allIds && ucastnici.allIds.length > 0) {
-    if (!ucastnici.invalidated) {
-      return; // Use cached value if not invalidated.
+    if (!connected || !ucastnici.invalidated) {
+      return; // Use cached value if: not connected or not invalidated.
     }
   }
 
