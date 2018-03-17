ca5a6c3a4060c7c4cb4cc09257c14f1c836c789b
Ivo Raisr <ivosh@ivosh.net>
2018-03-17 08:42:27 +0100

    Use CSS flexbox in PrihlaskyForm instead of Bootstrap Grid.

--- ui/src/registrator/Prihlasky/PrihlaskyForm/PrihlaskyForm.js
+++ ui/src/registrator/Prihlasky/PrihlaskyForm/PrihlaskyForm.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, Form, Glyphicon, Grid, Modal, Panel } from 'react-bootstrap';
+import { Button, Form, Glyphicon, Modal, Panel } from 'react-bootstrap';
 import HideableError from '../../../shared/HideableError';
 import LoadingButton from '../../../shared/LoadingButton';
 import PopisekKategorie from '../../../shared/Popisek/PopisekKategorie';
@@ -81,7 +81,7 @@ class PrihlaskyForm extends PureComponent {
     } = this.props;
 
     return (
-      <div className="PrihlaskyForm_div">
+      <div className="PrihlaskyForm__div">
         <Modal show={saved} onHide={onHideModal} bsSize="small">
           <Modal.Header closeButton>
             <Modal.Title>V pořádku</Modal.Title>
@@ -93,173 +93,167 @@ class PrihlaskyForm extends PureComponent {
             </Button>
           </Modal.Footer>
         </Modal>
-        <Grid fluid>
-          <Form
-            horizontal
-            onSubmit={this.handleSubmit}
-            autoComplete="off"
-            className="PrihlaskyForm_form"
-          >
-            <Col sm={4}>
-              <Panel bsStyle="primary" header="Údaje">
-                <PrihlaskyFormInputContainer
-                  index={0}
-                  inputRef={this.inputRef}
-                  name="udaje.prijmeni"
-                  popisek="příjmení"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={1}
-                  inputRef={this.inputRef}
-                  name="udaje.jmeno"
-                  popisek="jméno"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={2}
-                  inputRef={this.inputRef}
-                  name="udaje.narozeni"
-                  popisek="narození"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={3}
-                  inline={true}
-                  inputRef={this.inputRef}
-                  name="udaje.pohlavi"
-                  popisek="pohlaví"
-                  Type={RadioInput}
-                  Formatter={PopisekPohlavi}
-                />
-                <PrihlaskyFormInputContainer
-                  index={4}
-                  inputRef={this.inputRef}
-                  name="udaje.adresa"
-                  popisek="adresa"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={5}
-                  inputRef={this.inputRef}
-                  name="udaje.obec"
-                  popisek="obec"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={6}
-                  inputRef={this.inputRef}
-                  name="udaje.psc"
-                  popisek="PSČ"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={7}
-                  inputRef={this.inputRef}
-                  name="udaje.stat"
-                  popisek="stát"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={8}
-                  inputRef={this.inputRef}
-                  name="udaje.klub"
-                  popisek="klub"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={9}
-                  inputRef={this.inputRef}
-                  name="udaje.email"
-                  popisek="email"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={10}
-                  inputRef={this.inputRef}
-                  name="udaje.telefon"
-                  popisek="telefon"
-                  Type={TextInput}
-                />
-              </Panel>
-            </Col>
-            <Col sm={4}>
-              <Panel bsStyle="primary" header="Přihláška">
-                <PrihlaskyFormInputContainer
-                  index={11}
-                  inputRef={this.inputRef}
-                  name="prihlaska.datum"
-                  popisek="datum"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={12}
-                  inputRef={this.inputRef}
-                  name="prihlaska.typ"
-                  popisek="kategorie"
-                  Formatter={PopisekKategorie}
-                  Type={RadioInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={13}
-                  inputRef={this.inputRef}
-                  name="prihlaska.startCislo"
-                  popisek="číslo"
-                  Type={StartCisloInputContainer}
-                />
-                <PrihlaskyFormInputContainer
-                  index={14}
-                  inputRef={this.inputRef}
-                  name="prihlaska.kod"
-                  popisek="kód"
-                  Type={TextInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={15}
-                  inputRef={this.inputRef}
-                  name="ubytovani.pátek"
-                  popisek="ubytování"
-                  option="pátek"
-                  Type={CheckboxInput}
-                />
-                <PrihlaskyFormInputContainer
-                  index={16}
-                  inputRef={this.inputRef}
-                  name="ubytovani.sobota"
-                  popisek=""
-                  option="sobota"
-                  Type={TextInput}
-                />
-              </Panel>
-              <LoadingButton
-                type="submit"
-                bsStyle="success"
-                loading={saving}
-                loadingText="Probíhá ukládání..."
-              >
-                <Glyphicon glyph="save" />{' '}
-                {`Uložit ${existujiciUcastnik ? 'stávajícího' : 'nového'}
-                účastníka`}
-              </LoadingButton>
-              <Button bsStyle="danger" onClick={this.handleReset} className="PrihlaskyForm_reset">
-                <Glyphicon glyph="fire" /> Reset
-              </Button>
-            </Col>
-            <Col sm={4}>
-              <Panel bsStyle="primary" header="Platby">
-                <PlatbyContainer startIndex={17} inputRef={this.inputRef} />
-              </Panel>
-            </Col>
-          </Form>
-          {showError && (
-            <HideableError
-              code={errorCode}
-              message={errorMessage}
-              title="Chyba při ukládání!"
-              onHideError={onHideError}
+        <Form
+          horizontal
+          onSubmit={this.handleSubmit}
+          autoComplete="off"
+          className="PrihlaskyForm__form"
+        >
+          <Panel bsStyle="primary" className="PrihlaskyForm__column" header="Údaje">
+            <PrihlaskyFormInputContainer
+              index={0}
+              inputRef={this.inputRef}
+              name="udaje.prijmeni"
+              popisek="příjmení"
+              Type={TextInput}
             />
-          )}
-        </Grid>
+            <PrihlaskyFormInputContainer
+              index={1}
+              inputRef={this.inputRef}
+              name="udaje.jmeno"
+              popisek="jméno"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={2}
+              inputRef={this.inputRef}
+              name="udaje.narozeni"
+              popisek="narození"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={3}
+              inline={true}
+              inputRef={this.inputRef}
+              name="udaje.pohlavi"
+              popisek="pohlaví"
+              Type={RadioInput}
+              Formatter={PopisekPohlavi}
+            />
+            <PrihlaskyFormInputContainer
+              index={4}
+              inputRef={this.inputRef}
+              name="udaje.adresa"
+              popisek="adresa"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={5}
+              inputRef={this.inputRef}
+              name="udaje.obec"
+              popisek="obec"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={6}
+              inputRef={this.inputRef}
+              name="udaje.psc"
+              popisek="PSČ"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={7}
+              inputRef={this.inputRef}
+              name="udaje.stat"
+              popisek="stát"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={8}
+              inputRef={this.inputRef}
+              name="udaje.klub"
+              popisek="klub"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={9}
+              inputRef={this.inputRef}
+              name="udaje.email"
+              popisek="email"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={10}
+              inputRef={this.inputRef}
+              name="udaje.telefon"
+              popisek="telefon"
+              Type={TextInput}
+            />
+          </Panel>
+          <Panel bsStyle="primary" className="PrihlaskyForm__column" header="Přihláška">
+            <PrihlaskyFormInputContainer
+              index={11}
+              inputRef={this.inputRef}
+              name="prihlaska.datum"
+              popisek="datum"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={12}
+              inputRef={this.inputRef}
+              name="prihlaska.typ"
+              popisek="kategorie"
+              Formatter={PopisekKategorie}
+              Type={RadioInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={13}
+              inputRef={this.inputRef}
+              name="prihlaska.startCislo"
+              popisek="číslo"
+              Type={StartCisloInputContainer}
+            />
+            <PrihlaskyFormInputContainer
+              index={14}
+              inputRef={this.inputRef}
+              name="prihlaska.kod"
+              popisek="kód"
+              Type={TextInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={15}
+              inputRef={this.inputRef}
+              name="ubytovani.pátek"
+              popisek="ubytování"
+              option="pátek"
+              Type={CheckboxInput}
+            />
+            <PrihlaskyFormInputContainer
+              index={16}
+              inputRef={this.inputRef}
+              name="ubytovani.sobota"
+              popisek=""
+              option="sobota"
+              Type={TextInput}
+            />
+          </Panel>
+          <div className="PrihlaskyForm__column">
+            <Panel bsStyle="primary" header="Platby">
+              <PlatbyContainer startIndex={17} inputRef={this.inputRef} />
+            </Panel>
+            <LoadingButton
+              type="submit"
+              bsStyle="success"
+              loading={saving}
+              loadingText="Probíhá ukládání..."
+            >
+              <Glyphicon glyph="save" />{' '}
+              {`Uložit ${existujiciUcastnik ? 'stávajícího' : 'nového'}
+                účastníka`}
+            </LoadingButton>
+            <Button bsStyle="danger" onClick={this.handleReset} className="PrihlaskyForm__reset">
+              <Glyphicon glyph="fire" /> Reset
+            </Button>
+          </div>
+        </Form>
+        {showError && (
+          <HideableError
+            code={errorCode}
+            message={errorMessage}
+            title="Chyba při ukládání!"
+            onHideError={onHideError}
+          />
+        )}
       </div>
     );
   };
