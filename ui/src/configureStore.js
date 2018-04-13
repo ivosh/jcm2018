0532e25f6e11cdc36425a8e36b4772bddbf6354a
Ivo Raisr <ivosh@ivosh.net>
2018-04-13 13:39:29 +0200

    Remove demo startující. No longer necessary now that Startujici is fully working.

--- ui/src/configureStore.js
+++ ui/src/configureStore.js
@@ -6,30 +6,6 @@ import { websocketConnected, websocketDisconnected } from './App/AppActions';
 import { setHighestMezicasId } from './casomeric/Mezicasy/MezicasyActions';
 import { broadcastUcastnik } from './entities/ucastnici/ucastniciActions';
 
-const demoStartujiciState = {
-  startujici: [
-    { id: 0, cislo: 7, dokonceno: null },
-    { id: 1, cislo: 4, dokonceno: null },
-    { id: 17, cislo: 16, dokonceno: null },
-    { id: 2, cislo: 25, dokonceno: false },
-    { id: 3, cislo: 9, dokonceno: null },
-    { id: 12, cislo: 15, dokonceno: null },
-    { id: 9, cislo: 1, dokonceno: false },
-    { id: 29, cislo: 8, dokonceno: null },
-    { id: 5, cislo: 59, dokonceno: null },
-    { id: 11, cislo: 43, dokonceno: null },
-    { id: 18, cislo: 42, dokonceno: false },
-    { id: 10, cislo: 33, dokonceno: null },
-    { id: 7, cislo: 21, dokonceno: false },
-    { id: 13, cislo: 24, dokonceno: null },
-    { id: 34, cislo: 27, dokonceno: null },
-    { id: 14, cislo: 22, dokonceno: null },
-    { id: 58, cislo: 30, dokonceno: null },
-    { id: 15, cislo: 23, dokonceno: null },
-    { id: 59, cislo: 26, dokonceno: null }
-  ]
-};
-
 const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
@@ -74,7 +50,6 @@ const setupWsClient = (wsClient, store) => {
 
 const configureStore = (wsClient, initialStateParam = loadState()) => {
   const initialState = initialStateParam || {};
-  initialState.startujici = demoStartujiciState.startujici;
 
   if (initialState.casomeric && initialState.casomeric.mezicasy) {
     let highestId = 0;
