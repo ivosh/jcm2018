cfa7f3d6ffac03e3f5073467962f8d79f3772ab9
Ivo Raisr <ivosh@ivosh.net>
2018-05-02 19:12:12 +0200

    Broadcast only necessary attributes; not the whole mongoose object.

--- server/api/Stopky/broadcastStopky.js
+++ server/api/Stopky/broadcastStopky.js
@@ -2,9 +2,11 @@
 
 const Actions = require('../../../common/common');
 
-const broadcastStopky = async stopky => ({
-  broadcast: Actions.BROADCAST_STOPKY,
-  data: { ...stopky }
-});
+const broadcastStopky = async stopky => {
+  const { base, delta, running, typ } = stopky;
+  const data = { base, delta, running, typ };
+
+  return { broadcast: Actions.BROADCAST_STOPKY, data };
+};
 
 module.exports = broadcastStopky;
