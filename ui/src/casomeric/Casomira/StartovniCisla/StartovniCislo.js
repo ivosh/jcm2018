ee0b8e9213df16765d6e0bf68e7a881ef99c27c2
Ivo Raisr <ivosh@ivosh.net>
2018-05-09 17:28:02 +0200

    Show cursor:move for draggable StartovniCislo.

--- ui/src/casomeric/Casomira/StartovniCisla/StartovniCislo.js
+++ ui/src/casomeric/Casomira/StartovniCisla/StartovniCislo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { dokoncenoStr } from '../../../Util';
+import './StartovniCislo.css';
 
 const StartovniCislo = ({ startCislo, dokonceno, connectDragSource }) =>
   connectDragSource(
-    <div className={`StartovniCislaProTyp__item Legenda-item-${dokoncenoStr(dokonceno)[0]}`}>
+    <div
+      className={`StartovniCislo-casomeric StartovniCislaProTyp__item Legenda-item-${
+        dokoncenoStr(dokonceno)[0]
+      }`}
+    >
       {startCislo}
     </div>
   );
