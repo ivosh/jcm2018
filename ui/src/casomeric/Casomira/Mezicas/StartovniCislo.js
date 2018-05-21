714c7b1af909056721e6cee370ac3a1d8fb793cd
Ivo Raisr <ivosh@ivosh.net>
2018-05-21 17:06:36 +0200

    Implement isDropOver styling for StartovniCislo component.

--- ui/src/casomeric/Casomira/Mezicas/StartovniCislo.js
+++ ui/src/casomeric/Casomira/Mezicas/StartovniCislo.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './StartovniCislo.css';
 
-const StartovniCislo = ({ classNames, startCislo, connectDropTarget }) =>
-  connectDropTarget(<div className={classNames}>{startCislo}</div>);
+const StartovniCislo = ({ classNames, isDropOver, startCislo, connectDropTarget }) =>
+  connectDropTarget(
+    <div className={`${classNames} ${isDropOver ? 'casomeric-StartovniCislo--isDropOver' : ''}`}>
+      {startCislo}
+    </div>
+  );
 
 StartovniCislo.propTypes = {
   classNames: PropTypes.string.isRequired,
+  isDropOver: PropTypes.bool,
   startCislo: PropTypes.number,
   connectDropTarget: PropTypes.func.isRequired
 };
