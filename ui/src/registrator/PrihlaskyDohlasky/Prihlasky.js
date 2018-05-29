9b9a32082faa7beffcdc22b4f584231689b63abe
Ivo Raisr <ivosh@ivosh.net>
2018-05-29 14:21:55 +0200

    Pass actionPrefix and reduxName from the caller.

--- ui/src/registrator/PrihlaskyDohlasky/Prihlasky.js
+++ ui/src/registrator/PrihlaskyDohlasky/Prihlasky.js
@@ -22,30 +22,78 @@ PrihlaskyMainWithRoute.propTypes = {
 
 const PrihlaskyFormWithFetchUcastnici = withFetchUcastnici(PrihlaskyFormContainer);
 
-const PrihlaskyFormWithFetchUcastniciWithId = ({ match }) => (
-  <PrihlaskyFormWithFetchUcastnici actionPrefix="PRIHLASKY" loadId={match.params.id} reduxName="prihlasky" />
+const PrihlaskyFormWithFetchUcastniciWithId = ({ actionPrefix, match, reduxName }) => (
+  <PrihlaskyFormWithFetchUcastnici
+    actionPrefix={actionPrefix}
+    loadId={match.params.id}
+    reduxName={reduxName}
+  />
 );
 
 PrihlaskyFormWithFetchUcastniciWithId.propTypes = {
+  actionPrefix: PropTypes.string.isRequired,
   match: PropTypes.shape({
     params: PropTypes.object.isRequired
-  })
+  }),
+  reduxName: PropTypes.string.isRequired
 };
 
-const PrihlaskyFormWithFetchUcastniciWithEdit = () => <PrihlaskyFormWithFetchUcastnici actionPrefix="PRIHLASKY" reduxName="prihlasky" />;
-
-const PrihlaskyFormWithFetchUcastniciWithReset = () => (
-  <PrihlaskyFormWithFetchUcastnici actionPrefix="PRIHLASKY" reduxName="prihlasky" reset={true} />
+const PrihlaskyFormWithFetchUcastniciWithEdit = ({ actionPrefix, reduxName }) => (
+  <PrihlaskyFormWithFetchUcastnici actionPrefix={actionPrefix} reduxName={reduxName} />
 );
 
-const Prihlasky = ({ match }) => (
-  <Switch>
-    <Route path={`${match.path}/`} exact component={PrihlaskyMainWithRoute} />
-    <Route path={`${match.path}/edit`} component={PrihlaskyFormWithFetchUcastniciWithEdit} />
-    <Route path={`${match.path}/reset`} component={PrihlaskyFormWithFetchUcastniciWithReset} />
-    <Route path={`${match.path}/:id`} component={PrihlaskyFormWithFetchUcastniciWithId} />
-  </Switch>
+PrihlaskyFormWithFetchUcastniciWithEdit.propTypes = {
+  actionPrefix: PropTypes.string.isRequired,
+  reduxName: PropTypes.string.isRequired
+};
+
+const PrihlaskyFormWithFetchUcastniciWithReset = ({ actionPrefix, reduxName }) => (
+  <PrihlaskyFormWithFetchUcastnici actionPrefix={actionPrefix} reduxName={reduxName} reset={true} />
 );
+
+PrihlaskyFormWithFetchUcastniciWithReset.propTypes = {
+  actionPrefix: PropTypes.string.isRequired,
+  reduxName: PropTypes.string.isRequired
+};
+
+const Prihlasky = ({ match }) => {
+  const actionPrefix = 'PRIHLASKY';
+  const reduxName = 'prihlasky';
+
+  return (
+    <Switch>
+      <Route path={`${match.path}/`} exact component={PrihlaskyMainWithRoute} />
+      <Route
+        path={`${match.path}/edit`}
+        component={() => (
+          <PrihlaskyFormWithFetchUcastniciWithEdit
+            actionPrefix={actionPrefix}
+            reduxName={reduxName}
+          />
+        )}
+      />
+      <Route
+        path={`${match.path}/reset`}
+        component={() => (
+          <PrihlaskyFormWithFetchUcastniciWithReset
+            actionPrefix={actionPrefix}
+            reduxName={reduxName}
+          />
+        )}
+      />
+      <Route
+        path={`${match.path}/:id`}
+        component={matchProps => (
+          <PrihlaskyFormWithFetchUcastniciWithId
+            actionPrefix={actionPrefix}
+            reduxName={reduxName}
+            {...matchProps}
+          />
+        )}
+      />
+    </Switch>
+  );
+};
 
 Prihlasky.propTypes = {
   match: PropTypes.shape({
