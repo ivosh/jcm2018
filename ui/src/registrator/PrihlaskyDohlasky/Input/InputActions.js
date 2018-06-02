5897c870fa46c8844c3f0bc92d8888377eb0f2ce
Ivo Raisr <ivosh@ivosh.net>
2018-06-02 05:27:06 +0200

    Convert startovne to a number.

--- ui/src/registrator/PrihlaskyDohlasky/Input/InputActions.js
+++ ui/src/registrator/PrihlaskyDohlasky/Input/InputActions.js
@@ -1,8 +1,18 @@
 // eslint-disable-next-line import/prefer-default-export
-export const createInputChanged = actionPrefix => (name, event) => ({
-  type: `${actionPrefix}_INPUT_CHANGED`,
-  name,
-  id: event.target.id,
-  startovne: event.target.nonce,
-  value: event.target.type === 'checkbox' ? event.target.checked : event.target.value
-});
+export const createInputChanged = actionPrefix => (name, event) => {
+  const action = {
+    type: `${actionPrefix}_INPUT_CHANGED`,
+    name,
+    id: event.target.id,
+    value: event.target.type === 'checkbox' ? event.target.checked : event.target.value
+  };
+
+  if (event.target.nonce) {
+    const startovne = parseInt(event.target.nonce, 10);
+    if (!Number.isNaN(startovne)) {
+      action.startovne = startovne;
+    }
+  }
+
+  return action;
+};
