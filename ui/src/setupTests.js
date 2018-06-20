38b4f7c4039b07d9b76e16a4c82ab46a0e0eff22
Ivo Raisr <ivosh@ivosh.net>
2018-06-20 21:44:54 +0200

    Throw on uhandled promise rejections. Jest will return non-zero code eventually.

--- ui/src/setupTests.js
+++ ui/src/setupTests.js
@@ -6,3 +6,11 @@ import 'jest-localstorage-mock';
 /* eslint-enable import/no-extraneous-dependencies */
 
 configure({ adapter: new Adapter() });
+
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
+  process.on('unhandledRejection', reason => {
+    throw reason;
+  });
+  // Avoid memory leak by adding too many listeners.
+  process.env.LISTENING_TO_UNHANDLED_REJECTION = true;
+}
