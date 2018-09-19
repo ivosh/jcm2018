d9c949df6c359913ca8730a2857c28c88bd65d83
Ivo Raisr <ivosh@ivosh.net>
2018-09-19 09:51:09 +0200

    Introduce module findUcast. Rework createUcast to use it.

--- server/api/Ucastnik/Ucast/createUcast.js
+++ server/api/Ucastnik/Ucast/createUcast.js
@@ -1,53 +1,34 @@
 'use strict';
 
-const Actions = require('../../../../common/common');
+const { CODE_OK } = require('../../../../common/common');
 const logger = require('../../../logger');
 const Ucastnik = require('../../../model/Ucastnik/Ucastnik');
+const findUcast = require('./findUcast');
 
-/* Creates účast and účastník if necessary. Returns {code, ucastnik: Ucastnik instance}. */
+/* Creates účast and účastník if necessary. Returns {code, ucast, ucastnik}. */
 const createUcast = async ({ id, rok }) => {
+  let ucast = null;
   let ucastnik = null;
 
   if (!id) {
     ucastnik = new Ucastnik();
     logger.debug('Účastník created.');
   } else {
-    try {
-      ucastnik = await Ucastnik.findById(id);
-    } catch (err) {
-      logger.debug(`Účastník id ${id} not found.`);
-      return {
-        code: Actions.CODE_NONEXISTING,
-        status: `Účastník s id ${id} neexistuje. Detaily: ${err}`
-      };
+    const found = await findUcast({ id, rok });
+    ({ ucast, ucastnik } = found);
+    const { code, status } = found;
+    if (ucast) {
+      return { code, status, ucast, ucastnik };
     }
     if (!ucastnik) {
-      return { code: Actions.CODE_NONEXISTING, status: `Účastník s id ${id} neexistuje.` };
+      return { code, status };
     }
-
-    logger.debug(`Účastník id ${id} found.`);
-    logger.silly(`Účastník id ${id} found: ${ucastnik}`);
   }
 
-  const existujiciUcast = ucastnik.ucasti.find(ucast => ucast.rok === rok);
-  if (existujiciUcast) {
-    logger.debug(`Nalezena účast pro rok ${rok} účastníka ${ucastnik.id}.`);
-    return {
-      code: Actions.CODE_OK,
-      status: `Nalezena existující účast pro rok ${rok}.`,
-      ucast: existujiciUcast,
-      ucastnik
-    };
-  }
   logger.debug(`Vytvářím novou účast pro rok ${rok} účastníka ${ucastnik.id}.`);
   ucastnik.ucasti.push({ rok });
-  const novaUcast = ucastnik.ucasti.find(ucast => ucast.rok === rok);
-  return {
-    code: Actions.CODE_OK,
-    status: `Vytvořena účast pro rok ${rok}.`,
-    ucast: novaUcast,
-    ucastnik
-  };
+  ucast = ucastnik.ucasti.find(oneUcast => oneUcast.rok === rok);
+  return { code: CODE_OK, status: `Vytvořena účast pro rok ${rok}.`, ucast, ucastnik };
 };
 
 module.exports = createUcast;
---
+++ server/api/Ucastnik/Ucast/findUcast.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { CODE_OK, CODE_NONEXISTING } = require('../../../../common/common');
+const logger = require('../../../logger');
+const Ucastnik = require('../../../model/Ucastnik/Ucastnik');
+
+/* Finds an existing účast for an existing účastník. Returns {code, ucast, ucastnik}. */
+const findUcast = async ({ id, rok }) => {
+  let ucastnik = null;
+  try {
+    ucastnik = await Ucastnik.findById(id);
+  } catch (err) {
+    logger.debug(`Účastník id ${id} not found.`);
+    return { code: CODE_NONEXISTING, status: `Účastník s id ${id} neexistuje. Detaily: ${err}` };
+  }
+
+  if (!ucastnik) {
+    return { code: CODE_NONEXISTING, status: `Účastník s id ${id} neexistuje.` };
+  }
+  logger.debug(`Účastník id ${id} found.`);
+  logger.silly(`Účastník id ${id} found: ${ucastnik}`);
+
+  const ucast = ucastnik.ucasti.find(oneUcast => oneUcast.rok === rok);
+  if (ucast) {
+    logger.debug(`Nalezena účast pro rok ${rok} účastníka ${ucastnik.id}.`);
+    return {
+      code: CODE_OK,
+      status: `Nalezena existující účast pro rok ${rok}.`,
+      ucast,
+      ucastnik
+    };
+  }
+
+  logger.debug(`Účast roku ${rok} pro účastníka id ${id} not found.`);
+  return {
+    code: CODE_NONEXISTING,
+    status: `Účast roku ${rok} pro účastníka s id ${id} neexistuje.`,
+    ucastnik
+  };
+};
+
+module.exports = findUcast;
